a47eef212d18f8ad9369593086bfd13cd7dea19d
Alessandro <lsn.lodigiani@gmail.com>
2021-01-26 14:21:55 +0100

    Error.vue layout implementation

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1 style="font-size: 70px">ERROR <br> 404</h1>
+    </div>
+</template>
+
+<style>
+    .container {
+        margin: 0 auto;
+        min-height: 100vh;
+        justify-content: center;
+        align-content: center;
+        text-align: center;
+    }
+</style>
