520d8d3b295a037b61794a3dd1e8975731a2d87e
Alessandro <lsn.lodigiani@gmail.com>
2021-01-26 14:15:30 +0100

    Mobile menu Home link insert

--- components/Navigator/Sidenav.vue
+++ components/Navigator/Sidenav.vue
@@ -12,6 +12,7 @@
       <ul
         class="nav-list"
         @click="$emit('close')">
+        <li class="nav-item"><nuxt-link to="/">Home</nuxt-link></li>
         <li class="nav-item"><nuxt-link to="/posts">Blog</nuxt-link></li>
         <li class="nav-item"><nuxt-link to="/about">About</nuxt-link></li>
         <li class="nav-item"><nuxt-link to="/admin">Admin</nuxt-link></li>
