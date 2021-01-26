aeabb4a3f6ff97e3c2ee69f25175705b7c1a5d5f
Alessandro <lsn.lodigiani@gmail.com>
2021-01-26 11:39:48 +0100

    Correction and for cicle to create blog post page

---
+++ components/PostPreview.vue
@@ -0,0 +1,83 @@
+<!-- Questo componente genera la card per il preview dei post -->
+
+<template>
+    <div>
+        <!--{{ id }}-->
+        <!--{{ title }}-->
+        <!--{{ description }}-->
+        <!--{{ thumbnail }}-->
+        <!--{{ userImg }}-->
+        <!--{{ timeToRead }}-->
+
+        <!-- Codice di riferimento della singola card del post preview del tema Tailwind -->
+        <!-- 1- Inserisco nel codcie html i singoli props dove ne ho bisogno per rendere il componente dinamico -->
+			<div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
+				<a :href="`/posts/${id}`" class="flex flex-wrap no-underline hover:no-underline">
+					<img :src="thumbnail" :class=" `${ thumbnailFormat(tail) } w-full rounded-t pb-6` ">
+					<p class="w-full text-gray-600 text-xs md:text-sm px-6">GETTING STARTED</p>
+					<div class="w-full font-bold text-xl text-gray-900 px-6"> {{ title }} </div>
+					<p class="text-gray-800 font-serif text-base px-6 mb-5">
+						{{ description }}
+					</p>
+				</a>
+			</div>
+			<div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
+				<div class="flex items-center justify-between">
+					<img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" :src="userImg" alt="Avatar of Author" tabindex="0">
+					<p class="text-gray-600 text-xs md:text-sm"> {{ timeToRead }} </p>
+				</div>
+			</div>
+
+
+		
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PostPreview',
+    data(){
+        return {
+
+        }
+    },
+    methods : {
+        thumbnailFormat(tail){
+            return tail == '1/3' ? 'h-64' : 'h-full'
+        }
+    },
+    //i props sono le proprietà del componente che accettano input dall'esterno, quindi possiamo richiamare il componente dall'esterno per avere determinati parametri
+    props: {
+        id : {
+            type : String,
+            required : true
+        },
+        title : {
+            type : String,
+            required : true
+        },
+        description : {
+            type : String,
+            required : true
+        },
+        thumbnail : {
+            type : String,
+            required : true
+        },
+        userImg : {
+            type : String,
+            required : true
+        },
+        timeToRead : {
+            type : String,
+            required : true
+        },
+        tail : {
+            type : String,
+            required : true
+        }
+    }
+
+}
+</script>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -12,155 +12,50 @@
 
         <!-- Codice per i post del tema di Tailwind -->
         <div class="flex flex-wrap justify-between pt-12 -mx-6">
-
-					<!--1/3 col -->
-					<div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-						<div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-							<a href="#" class="flex flex-wrap no-underline hover:no-underline">
-								<img src="https://source.unsplash.com/collection/225/800x600" class="h-64 w-full rounded-t pb-6">
-								<p class="w-full text-gray-600 text-xs md:text-sm px-6">GETTING STARTED</p>
-								<div class="w-full font-bold text-xl text-gray-900 px-6">Lorem ipsum dolor sit amet.</div>
-								<p class="text-gray-800 font-serif text-base px-6 mb-5">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula. 
-								</p>
-							</a>
-						</div>
-						<div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-							<div class="flex items-center justify-between">
-								<img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author" tabindex="0">
-								<p class="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
-							</div>
-						</div>
-					</div>
-				
-					
-					<!--1/3 col -->
-					<div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-						<div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-							<a href="#" class="flex flex-wrap no-underline hover:no-underline">
-								<img src="https://source.unsplash.com/collection/3106804/800x600" class="h-64 w-full rounded-t pb-6">
-								<p class="w-full text-gray-600 text-xs md:text-sm px-6">GETTING STARTED</p>
-								<div class="w-full font-bold text-xl text-gray-900 px-6">Lorem ipsum dolor sit amet.</div>
-								<p class="text-gray-800 font-serif text-base px-6 mb-5">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ip Aliquam at ipsum eu nunc commodo posuere et sit amet ligula. 
-								</p>
-							</a>
-							</div>
-						<div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-							<div class="flex items-center justify-between">
-								<img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author" tabindex="0">
-								<p class="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
-							</div>
-						</div>
-					</div>
-
-					<!--1/3 col -->
-					<div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-						<div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-							<a href="#" class="flex flex-wrap no-underline hover:no-underline">
-								<img src="https://source.unsplash.com/collection/539527/800x600" class="h-64 w-full rounded-t pb-6">
-								<p class="w-full text-gray-600 text-xs md:text-sm px-6">GETTING STARTED</p>
-								<div class="w-full  font-bold text-xl text-gray-900 px-6">Lorem ipsum dolor sit amet.</div>
-								<p class="text-gray-800 font-serif text-base px-6 mb-5">
-									Lorem ipsum eu nunc commodo posuere et sit amet ligula. 
-								</p>
-							</a>
-						</div>
-						<div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-							<div class="flex items-center justify-between">
-								<img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author" tabindex="0">
-								<p class="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
-							</div>
-						</div>
-					</div>
-
-
-					<!--1/2 col -->
-					<div class="w-full md:w-1/2 p-6 flex flex-col flex-grow flex-shrink">
-						<div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-							<a href="#" class="flex flex-wrap no-underline hover:no-underline">
-								<img src="https://source.unsplash.com/collection/3657445/800x600" class="h-full w-full rounded-t pb-6">
-								<p class="w-full text-gray-600 text-xs md:text-sm px-6">GETTING STARTED</p>
-								<div class="w-full font-bold text-xl text-gray-900 px-6">Lorem ipsum dolor sit amet.</div>
-								<p class="text-gray-800 font-serif text-base px-6 mb-5">
-									Lorem ipsum eu nunc commodo posuere et sit amet ligula. 
-								</p>
-							</a>
-						</div>
-						<div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-							<div class="flex items-center justify-between">
-								<img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author" tabindex="0">
-								<p class="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
-							</div>
-						</div>
-					</div>
-
-					<!--1/2 col -->
-					<div class="w-full md:w-1/2 p-6 flex flex-col flex-grow flex-shrink">
-						<div class="flex-1 flex-row bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-							<a href="#" class="flex flex-wrap no-underline hover:no-underline">	
-								<img src="https://source.unsplash.com/collection/764827/800x600" class="h-full w-full rounded-t pb-6">
-								<p class="w-full text-gray-600 text-xs md:text-sm px-6">GETTING STARTED</p>
-								<div class="w-full font-bold text-xl text-gray-900 px-6">Lorem ipsum dolor sit amet.</div>
-								<p class="text-gray-800 font-serif text-base px-6 mb-5">
-									Lorem ipsum eu nunc commodo posuere et sit amet ligula. 
-								</p>
-							</a>
-						</div>
-						<div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-							<div class="flex items-center justify-between">
-								<img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author" tabindex="0">
-								<p class="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
-							</div>
-						</div>
-					</div>
-
-
-
-					<!--2/3 col -->
-					<div class="w-full md:w-2/3 p-6 flex flex-col flex-grow flex-shrink">
-						<div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-							<a href="#" class="flex flex-wrap no-underline hover:no-underline">	
-								<img src="https://source.unsplash.com/collection/325867/800x600" class="h-full w-full rounded-t pb-6">
-								<p class="w-full text-gray-600 text-xs md:text-sm px-6">GETTING STARTED</p>
-								<div class="w-full font-bold text-xl text-gray-900 px-6">Lorem ipsum dolor sit amet.</div>
-								<p class="text-gray-800 font-serif text-base px-6 mb-5">
-									Lorem ipsum eu nunc commodo posuere et sit amet ligula. 
-								</p>
-							</a>
-						</div>
-						<div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-							<div class="flex items-center justify-between">
-								<img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author" tabindex="0">
-								<p class="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
-							</div>
-						</div>
-					</div>
-
-					<!--1/3 col -->
-					<div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-						<div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-							<a href="#" class="flex flex-wrap no-underline hover:no-underline">
-								<img src="https://source.unsplash.com/collection/1118905/800x600" class="h-full w-full rounded-t pb-6">
-								<p class="w-full text-gray-600 text-xs md:text-sm px-6">GETTING STARTED</p>
-								<div class="w-full font-bold text-xl text-gray-900 px-6">Lorem ipsum dolor sit amet.</div>
-								<p class="text-gray-800 font-serif text-base px-6 mb-5">
-									Lorem ipsum eu nunc commodo posuere et sit amet ligula. 
-								</p>
-							</a>
-						</div>
-						<div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-							<div class="flex items-center justify-between">
-								<img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author" tabindex="0">
-								<p class="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
-							</div>
-						</div>
-					</div>
-
-				</div>
+			<!-- All'interno di questo div "row" inseriamo un ciclo for per ciclare il nostro components -->
+			<div v-for="(post, index) in posts" :key=" `__post_${index}` " :class=" `w-full md:w-${post.tail} p-6 flex flex-col flex-grow flex-shrink` ">
+					<!-- Inserimento del componente PostPreview.vue -->
+					<!-- 1- Bisogna passare le proprietà dei props del componente all'interno del tag -->
+					<!-- Per renderlo dinamico il componentne in base all'array devo usare il v-bind e richiamare le prorpietà dell'array -->
+					<PostPreview
+						:id="post.id"
+						:title="post.title"
+						:description="post.description"
+						:thumbnail="post.thumbnail"
+						:userImg="post.userImg"
+						:timeToRead="post.timeToRead"
+						:tail="post.tail"
+					>
+					</PostPreview>
+			</div>
+		</div>
 
                 <!-- Fine Codice tema Tailwind -->
 
 
     </div>
 </template>
+
+<script>
+import PostPreview from '~/components/PostPreview.vue'
+
+export default {
+	components : {
+		PostPreview
+	},
+	data() {
+		return {
+			posts : [
+				{ id : '1', tail : '1/3', title : 'Lorem ipsum dolor sit amet.', description : 'Lorem ipsum eu nunc commodo posudue', thumbnail : 'https://source.unsplash.com/collection/225/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1MIN READ'},
+				{ id : '2', tail : '1/3', title : 'Lorem ipsum dolor sit amet.', description : 'Lorem ipsum eu nunc commodo posudue', thumbnail : 'https://source.unsplash.com/collection/3106804/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1MIN READ'},
+				{ id : '3', tail : '1/3', title : 'Lorem ipsum dolor sit amet.', description : 'Lorem ipsum eu nunc commodo posudue', thumbnail : 'https://source.unsplash.com/collection/3657445/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1MIN READ'},
+				{ id : '4', tail : '1/2', title : 'Lorem ipsum dolor sit amet.', description : 'Lorem ipsum eu nunc commodo posudue', thumbnail : 'https://source.unsplash.com/collection/764827/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1MIN READ'},
+				{ id : '5', tail : '1/2', title : 'Lorem ipsum dolor sit amet.', description : 'Lorem ipsum eu nunc commodo posudue', thumbnail : 'https://source.unsplash.com/collection/1118905/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1MIN READ'},
+				{ id : '6', tail : '1/3', title : 'Lorem ipsum dolor sit amet.', description : 'Lorem ipsum eu nunc commodo posudue', thumbnail : 'https://source.unsplash.com/collection/325867/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1MIN READ'},
+				{ id : '6', tail : '1/3', title : 'Lorem ipsum dolor sit amet.', description : 'Lorem ipsum eu nunc commodo posudue', thumbnail : 'https://source.unsplash.com/collection/325867/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1MIN READ'},
+				{ id : '6', tail : '1/3', title : 'Lorem ipsum dolor sit amet.', description : 'Lorem ipsum eu nunc commodo posudue', thumbnail : 'https://source.unsplash.com/collection/325867/800x600', userImg : 'http://i.pravatar.cc/300', timeToRead : '1MIN READ'}
+			]
+		}
+	}
+}
+</script>
